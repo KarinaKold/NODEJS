e96c0dbbf3e019df358944760be7aa97612cfb2f
Karina Koldaeva <karina.mila@yandex.ru>
2026-03-02 22:00:58 +0300

    write/read data

---
+++ index.js
@@ -0,0 +1,39 @@
+// node index add --title=Hello
+// node index list
+
+/*-----------------------------*/
+
+const yargs = require("yargs");
+
+const pkg = require("./package.json");
+yargs.version(pkg.version);
+
+const { addNote, getNotes } = require("./notes.controller");
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    // console.log("Add command", title);
+    addNote(title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    // console.log("List command");
+    const notes = await getNotes();
+    console.log(notes);
+  },
+});
+
+yargs.parse();
---
+++ notes.controller.js
@@ -0,0 +1,39 @@
+// const notes = [];
+
+const fs = require("fs/promises");
+const path = require("path");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  // const notes = require("./db.json"); // 1
+
+  // const notes = await fs.readFile(notesPath); // 2
+  // Buffer.from(notes).toString("utf-8");
+
+  // const buffer = await fs.readFile(notesPath); // 3
+  // const notes = Buffer.from(buffer).toString("utf-8");
+
+  // const notes = await fs.readFile(notesPath, { encoding: "utf-8" }); // 4
+  const notes = await getNotes();
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+  notes.push(note);
+
+  // await fs.writeFile("./db.json", JSON.stringify(notes));
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+
+async function getNotes() {
+  // return notes;
+  // return require('./db.json')
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+module.exports = {
+  addNote,
+  getNotes,
+};
