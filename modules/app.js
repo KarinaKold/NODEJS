f01a7a9152197313bb6d7039f43a5ed21916b32e
Karina Koldaeva <karina.mila@yandex.ru>
2026-03-03 21:08:00 +0300

    modules

---
+++ modules/app.js
@@ -0,0 +1,12 @@
+/* ES modules */
+
+// import {Playground} from './playground.js'
+// import path from "path";
+// console.log(path);
+
+// import { fileUrlToPath } from "url";
+// const __filename = fileUrlToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
+console.log(__filename);
