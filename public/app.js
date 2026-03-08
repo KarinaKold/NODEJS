062af6fbc8616f3578bd91e516c4f9d2b4231ae7
Karina Koldaeva <karina.mila@yandex.ru>
2026-03-08 20:14:59 +0300

    feat: add editing title in input field with save&cancel actions

--- public/app.js
+++ public/app.js
@@ -10,22 +10,62 @@ document.addEventListener("click", (event) => {
     const id = event.target.dataset.id;
     const title = event.target.dataset.title;
 
-    const promptTitle = prompt("Введите новое название", title);
+    const li = event.target.closest("li");
+    const buttons = li.querySelector("div");
 
-    if (promptTitle) {
-      edit(id, promptTitle).then(() => {
-        document.querySelector("li").textContent = promptTitle;
-      });
-    }
+    const editContainer = document.createElement("div");
+    editContainer.classList.add("d-flex", "justify-content-between", "w-100");
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = title;
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttonContainer");
+
+    editContainer.append(input, buttonContainer);
+
+    const saveButton = document.createElement("button");
+    saveButton.classList.add("btn", "btn-success");
+    saveButton.textContent = "Сохранить";
+
+    const cancelButton = document.createElement("button");
+    cancelButton.classList.add("btn", "btn-danger", "ms-1");
+    cancelButton.textContent = "Отменить";
+
+    buttonContainer.append(saveButton, cancelButton);
+
+    li.innerHTML = "";
+    li.appendChild(editContainer);
+
+    saveButton.addEventListener("click", () => {
+      const newTitle = input.value.trim();
+
+      if (newTitle && newTitle !== title) {
+        editNote(id, newTitle).then(() => {
+          event.target.dataset.title = newTitle;
+          li.innerHTML = "";
+          li.append(newTitle, buttons);
+        });
+      } else {
+        li.innerHTML = "";
+        li.append(title, buttons);
+      }
+    });
+
+    cancelButton.addEventListener("click", () => {
+      li.innerHTML = "";
+      li.append(title, buttons);
+    });
   }
 });
 
 async function remove(id) {
-  fetch(`/${id}`, { method: "DELETE" });
+  await fetch(`/${id}`, { method: "DELETE" });
 }
 
-async function edit(id, title) {
-  fetch(`/${id}`, {
+async function editNote(id, title) {
+  await fetch(`/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
